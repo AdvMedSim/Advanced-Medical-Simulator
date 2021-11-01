9eb980e3144ba646146ece4cce9413a4c9841c43
AdvMedSim <medicalsimdevelopment@gmail.com>
2021-11-01 17:21:22 +0100

    Add Module class
    Adds the core Module class.

---
+++ Core/Module.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Module class is the base class for all custom Modules/Systems/Components.
+/// </summary>
+public abstract class Module : MonoBehaviour
+{
+    /// <summary>
+    /// Call to log the position, rotation and the local scale of this Module to the Console.
+    /// </summary>
+    public void DebugModule()
+    {
+        // Log the position.
+        Debug.Log(transform.position);
+
+        // Log the rotation.
+        Debug.Log(transform.rotation);
+
+        // Log the local scale.
+        Debug.Log(transform.localScale);
+    }
+}
