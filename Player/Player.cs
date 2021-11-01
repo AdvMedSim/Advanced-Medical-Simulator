14f57e877d35d02657fc333f9f147e9ef04c3358
AdvMedSim <medicalsimdevelopment@gmail.com>
2021-11-01 17:36:00 +0100

    Add Player class
    Adds the core Player class.

---
+++ Player/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Modules.Creatures.Humans
+{
+    /// <summary>
+    /// The Player class handles movement, interaction and functional game logic.
+    /// </summary>
+    public class Player : Human
+    {
+
+    }
+}
