d3cb020c28932d79991d0d8dd8d9a575771e9a34
AdvMedSim <medicalsimdevelopment@gmail.com>
2021-11-01 17:34:04 +0100

    Add Human class
    Adds the Human class which is the base class for all Human Types.

---
+++ Creatures/Human.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Modules.Creatures.Humans
+{
+    /// <summary>
+    /// The Human class is the base class for all Human Types.
+    /// </summary>
+    public abstract class Human : Creature
+    {
+
+    }
+}
