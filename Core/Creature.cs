b488ebc0a9aa89c2656d0c2cedc722e9579e8caf
AdvMedSim <medicalsimdevelopment@gmail.com>
2021-11-01 17:33:38 +0100

    Add Creature class
    Adds the Creature class which is the base class for all Creature Types.

---
+++ Core/Creature.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Modules.Creatures
+{
+    /// <summary>
+    /// The Creature class is the base class for all Creature Types.
+    /// </summary>
+    public abstract class Creature : Module
+    {
+
+    }
+}
