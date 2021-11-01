a121e5c9482f844254057866575d3c5b924f1054
AdvMedSim <medicalsimdevelopment@gmail.com>
2021-11-01 17:33:14 +0100

    Update Module namespace
    Updates the Module class to use the appropriate namespace.

--- Core/Module.cs
+++ Core/Module.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// The Module class is the base class for all custom Modules/Systems/Components.
-/// </summary>
-public abstract class Module : MonoBehaviour
+namespace Game.Modules
 {
     /// <summary>
-    /// Call to log the position, rotation and the local scale of this Module to the Console.
+    /// The Module class is the base class for all custom Modules/Systems/Components.
     /// </summary>
-    public void DebugModule()
+    public abstract class Module : MonoBehaviour
     {
-        // Log the position.
-        Debug.Log(transform.position);
+        /// <summary>
+        /// Call to log the position, rotation and the local scale of this Module to the Console.
+        /// </summary>
+        public void DebugModule()
+        {
+            // Log the position.
+            Debug.Log(transform.position);
 
-        // Log the rotation.
-        Debug.Log(transform.rotation);
+            // Log the rotation.
+            Debug.Log(transform.rotation);
 
-        // Log the local scale.
-        Debug.Log(transform.localScale);
+            // Log the local scale.
+            Debug.Log(transform.localScale);
+        }
     }
 }
