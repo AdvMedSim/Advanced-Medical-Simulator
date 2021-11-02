54c3fabb6d6f796dd4d06a1304fff4f6f075b951
AdvMedSim <medicalsimdevelopment@gmail.com>
2021-11-02 13:45:01 +0100

    Add Transform Extension class
    Adds the Transform Extension class which is an Extension Type focused on Transform manipulation.

---
+++ Extensions/Core/TransformExtension.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Modules.Extensions
+{
+    /// <summary>
+    /// The Transform Extension class is the base class for all Extension Types which focus on Transform manipulation.
+    /// </summary>
+    public abstract class TransformExtension : Extension
+    {
+
+    }
+}
